6848b6eeb71a34e76d02a564dc492631f9b438c2
sjankdev <stefanjanko.dev@gmail.com>
2024-10-09 17:22:59 +0200

    Add email/password login functionality alongside Google sign-in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Registration from './components/Registration';
 import Home from './components/Home'; 
 
+import Login from './components/Login';  
+
 const App: React.FC = () => {
     return (
         <Router>
             <div>
                 <h1>Welcome to AttendIt</h1>
                 <Routes>
-                    <Route path="/" element={<Registration />} /> 
+                    <Route path="/" element={<Registration />} />
+                    <Route path="/login" element={<Login />} />  
                     <Route path="/home" element={<Home />} />
                 </Routes>
             </div>
